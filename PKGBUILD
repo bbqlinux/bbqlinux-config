76ba91a8cb3ed661c925bc4085a6a3dae71f96b5
Daniel Hillenbrand <codeworkx@cyanogenmod.org>
2013-04-06 16:45:30 +0000

    use autostart to init bbqlinux gsettings

--- PKGBUILD
+++ PKGBUILD
@@ -1,17 +1,20 @@
 # Maintainer: Daniel Hillenbrand <codeworkx@bbqlinux.org>
 
 pkgname=bbqlinux-config
-pkgver=1.2.0
+pkgver=1.2.1
 pkgrel=1
 pkgdesc="BBQLinux Configuration"
 arch=('any')
-depends=('bbqlinux-artwork')
+depends=('bbqlinux-artwork>=1.0.3')
 url="https://github.com/bbqlinux/bbqlinux-config"
 license=('GPL')
 
 package() {
     cd "$pkgdir"
     mkdir -p etc
+    mkdir -p usr/bin
+
+    install -Dm755 "$srcdir/usr/bin/bbqlinux-init-gsettings.sh" usr/bin/bbqlinux-init-gsettings.sh
 
     cp -R "$srcdir/etc/skel" etc/skel
 }
---
+++ src/usr/bin/bbqlinux-init-gsettings.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/bash
+
+if [ ! -f ~/.bbqlinux_gsettings ]
+then
+    # theme
+    gsettings set org.mate.interface gtk-theme 'Shiki-Brave'
+    gsettings set org.mate.interface icon-theme 'gnome-brave'
+
+    # wallpaper
+    gsettings set org.mate.background picture-filename '/usr/share/backgrounds/bbqlinux/bbqlinux_wallpaper_00.jpg'
+
+    # font rendering
+    gsettings set org.mate.font-rendering antialiasing 'rgba'
+    gsettings set org.mate.font-rendering dpi 96.0
+    gsettings set org.mate.font-rendering hinting 'slight'
+    gsettings set org.mate.font-rendering rgba-order 'rgb'
+
+    # mate terminal
+    gsettings set org.mate.terminal.profile:/org/mate/terminal/profiles/default/ default-size-columns 100
+    gsettings set org.mate.terminal.profile:/org/mate/terminal/profiles/default/ default-size-rows 30
+    gsettings set org.mate.terminal.profile:/org/mate/terminal/profiles/default/ use-custom-default-size true
+    gsettings set org.mate.terminal.profile:/org/mate/terminal/profiles/default/ background-color '#2C892C892C89'
+    gsettings set org.mate.terminal.profile:/org/mate/terminal/profiles/default/ foreground-color '#C420C420C420'
+    gsettings set org.mate.terminal.profile:/org/mate/terminal/profiles/default/ bold-color '#000000000000'
+    gsettings set org.mate.terminal.profile:/org/mate/terminal/profiles/default/ palette '#2E2E34343636:#CCCC00000000:#4E4E9A9A0606:#C4C4A0A00000:#34346565A4A4:#757550507B7B:#060698209A9A:#D3D3D7D7CFCF:#555557575353:#EFEF29292929:#8A8AE2E23434:#FCFCE9E94F4F:#72729F9FCFCF:#ADAD7F7FA8A8:#3434E2E2E2E2:#EEEEEEEEECEC'
+    gsettings set org.mate.terminal.profile:/org/mate/terminal/profiles/default/ use-theme-colors false
+	gsettings set org.mate.terminal.profile:/org/mate/terminal/profiles/default/ scrollback-unlimited true
+	gsettings set org.mate.terminal.profile:/org/mate/terminal/profiles/default/ visible-name 'Default'
+
+    touch ~/.bbqlinux_gsettings
+    echo $(date +"%Y%m%d") > ~/.bbqlinux_gsettings
+fi
+
+exit 0

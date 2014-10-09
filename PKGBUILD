7d3e7c6304fffec8817a7300770dc1d70ea0edf8
Daniel Hillenbrand <codeworkx@cyanogenmod.org>
2014-10-09 21:22:45 +0200

    add bbqlinux specific pacman config

--- PKGBUILD
+++ PKGBUILD
@@ -1,11 +1,11 @@
-# Maintainer: Daniel Hillenbrand <codeworkx@bbqlinux.org>
+# Maintainer: Daniel Hillenbrand < codeworkx [at] bbqlinux [dot] org>
 
 pkgname=bbqlinux-config
-pkgver=1.3.2
+pkgver=1.4.0
 pkgrel=1
 pkgdesc="BBQLinux Configuration"
 arch=('any')
-depends=('bbqlinux-artwork>=1.0.3' 'bbqlinux-theme')
+depends=('bbqlinux-mirrorlist' 'bbqlinux-artwork>=1.0.3' 'bbqlinux-theme')
 url="https://github.com/bbqlinux/bbqlinux-config"
 license=('GPL')
 install=${pkgname}.install
@@ -14,6 +14,11 @@ package() {
     cd "$pkgdir"
     mkdir -p etc
     mkdir -p usr/bin
+
+    install -Dm644 "$srcdir/etc/pacman.bbqlinux.conf" etc/pacman.bbqlinux.conf
+    install -Dm644 "$srcdir/etc/pacman.i686.conf.bbqnew" etc/pacman.i686.conf.bbqnew
+    install -Dm644 "$srcdir/etc/pacman.x86_64.conf.bbqnew" etc/pacman.x86_64.conf.bbqnew
+
 
     install -Dm755 "$srcdir/usr/bin/bbqlinux-gsettings.sh" usr/bin/bbqlinux-gsettings.sh
 
--- src/usr/bin/bbqlinux-gsettings.sh
+++ src/usr/bin/bbqlinux-gsettings.sh
@@ -4,7 +4,7 @@ echo "> BBQLinux GSettings"
 
 mkdir -p ~/.bbqlinux
 
-NEW_VERSION=20141003
+NEW_VERSION=20141005
 if [ ! -f ~/.bbqlinux/.gsettings ] || [ "$1" == "init" ]
 then
     echo "Applying initial settings."
@@ -28,11 +28,7 @@ then
     gsettings set org.mate.terminal.profile:/org/mate/terminal/profiles/default/ default-size-columns 100
     gsettings set org.mate.terminal.profile:/org/mate/terminal/profiles/default/ default-size-rows 30
     gsettings set org.mate.terminal.profile:/org/mate/terminal/profiles/default/ use-custom-default-size true
-    gsettings set org.mate.terminal.profile:/org/mate/terminal/profiles/default/ background-color '#2C892C892C89'
-    gsettings set org.mate.terminal.profile:/org/mate/terminal/profiles/default/ foreground-color '#C420C420C420'
-    gsettings set org.mate.terminal.profile:/org/mate/terminal/profiles/default/ bold-color '#000000000000'
-    gsettings set org.mate.terminal.profile:/org/mate/terminal/profiles/default/ palette '#2E2E34343636:#CCCC00000000:#4E4E9A9A0606:#C4C4A0A00000:#34346565A4A4:#757550507B7B:#060698209A9A:#D3D3D7D7CFCF:#555557575353:#EFEF29292929:#8A8AE2E23434:#FCFCE9E94F4F:#72729F9FCFCF:#ADAD7F7FA8A8:#3434E2E2E2E2:#EEEEEEEEECEC'
-    gsettings set org.mate.terminal.profile:/org/mate/terminal/profiles/default/ use-theme-colors false
+    gsettings set org.mate.terminal.profile:/org/mate/terminal/profiles/default/ use-theme-colors true
     gsettings set org.mate.terminal.profile:/org/mate/terminal/profiles/default/ scrollback-unlimited true
     gsettings set org.mate.terminal.profile:/org/mate/terminal/profiles/default/ visible-name 'Default'
 
@@ -43,7 +39,7 @@ fi
 
 CURRENT_VERSION=$(cat ~/.bbqlinux/.gsettings)
 NEW_VERSION=20141003
-if [ ${CURRENT_VERSION} -lt ${NEW_VERSION} ] || [ "$1" == "init" ]
+if [ ${CURRENT_VERSION} -lt ${NEW_VERSION} ]
 then
     echo "Upgrading from ${CURRENT_VERSION} to ${NEW_VERSION}"
 
@@ -53,6 +49,19 @@ then
 
 	# Marco
     gsettings set org.mate.Marco.general theme 'BBQLinux'
+
+    echo ${NEW_VERSION} > ~/.bbqlinux/.gsettings
+	echo "Done."
+fi
+
+CURRENT_VERSION=$(cat ~/.bbqlinux/.gsettings)
+NEW_VERSION=20141005
+if [ ${CURRENT_VERSION} -lt ${NEW_VERSION} ]
+then
+    echo "Upgrading from ${CURRENT_VERSION} to ${NEW_VERSION}"
+
+    # Mate Terminal
+    gsettings set org.mate.terminal.profile:/org/mate/terminal/profiles/default/ use-theme-colors true
 
     echo ${NEW_VERSION} > ~/.bbqlinux/.gsettings
 	echo "Done."
